26a76bef492d09c3e8556406b955121e96e0ee1b
harshgupta1064 <harshgupta1064@gmail.com>
2025-02-23 08:36:55 +0530

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
+
+
 }
 
 android {
@@ -45,4 +48,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+
+    // RecyclerView for displaying tarot cards
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+
+    // Room Database for saving past readings
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
+
+    // ViewModel and LiveData for state management
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+    // Lottie for animations (Used later)
+    implementation("com.airbnb.android:lottie:6.0.0")
+
 }
