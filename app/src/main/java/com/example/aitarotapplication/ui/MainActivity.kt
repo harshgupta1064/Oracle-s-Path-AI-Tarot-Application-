dfc3dd26bb09b3f822a54ebea8850a4c224f680d
harshgupta1064 <harshgupta1064@gmail.com>
2025-02-23 10:38:08 +0530

    First Commit

--- app/src/main/java/com/example/aitarotapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/aitarotapplication/ui/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.aitarotapplication.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aitarotapplication.R
+import com.example.aitarotapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        val nextButton = binding.nextButton;
+        val message = binding.message;
+        nextButton.setOnClickListener {
+            val userQuestion = message.text.toString().trim()
+
+            if (userQuestion.isNotEmpty()) {
+                val intent = Intent(this, TarotCards::class.java)
+                intent.putExtra("userQuestion", userQuestion) // Pass question to next screen
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please enter a message.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/aitarotapplication/ui/SplashScreen.kt
+++ app/src/main/java/com/example/aitarotapplication/ui/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.example.aitarotapplication.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,10 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
